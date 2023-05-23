eee2cbc9ec09f561a7f762fa0cad3cd55bcc0f2d
CaNaRdEoS <125059936+CaNaRdEoS@users.noreply.github.com>
2023-05-23 16:08:03 +0200

    Quoi
    Feur

---
+++ Tomato/src/modèle/Couleur.java
@@ -0,0 +1,25 @@
+package modèle;
+
+public enum Couleur {
+
+	BLEU ("Bleu"), VERT("Vert"), ROUGE("Rouge"), ORANGE("Orange"), JAUNE("Jaune"), NOIR("Noir"), MULTICOLORE("Multicolore");
+	private final String dénomination;
+
+	private Couleur(String dénomination) {
+		this.dénomination  = dénomination;
+	}
+
+	public String getDénomination() {
+		return this.dénomination;
+	}
+	
+	public static Couleur getCouleur(String dénomination) {
+		for (Couleur c : Couleur.values()) {
+			if (c.getDénomination().equals(dénomination)) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+}
