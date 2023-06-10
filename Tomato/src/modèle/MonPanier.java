66410693cb640c4a3ef3a89a30b4abd397775b3a
CaNaRdEoS <125059936+CaNaRdEoS@users.noreply.github.com>
2023-06-10 19:04:12 +0200

    Create MonPanier.java
    Class pour manipuler son panier

---
+++ Tomato/src/modèle/MonPanier.java
@@ -0,0 +1,46 @@
+package modèle;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MonPanier {
+
+	private List<Tomate> tomatesDuPanier;
+	
+	public MonPanier() {
+		this.tomatesDuPanier = new LinkedList<Tomate>();
+	}
+	
+	public List<Tomate> getMesTomates() {
+		return this.tomatesDuPanier;
+	}
+	
+	public void addMesTomate(Tomate tomate,int quantite) {
+		for (int i=0;i<quantite;i++)
+			this.tomatesDuPanier.add(tomate);
+	}
+	
+	public void clearMesTomates() {
+		this.tomatesDuPanier.clear();
+	}
+	
+	public int sizehMesTomates() {
+		return this.tomatesDuPanier.size();
+	}
+	
+	public float prixMonPanier() {
+		float prix = 0F;
+		for (Tomate tomate: this.getMesTomates()) {
+			prix+=tomate.getPrixTTC();
+		}
+		return prix;
+	}
+	
+	public Tomate getMaTomate(String designation) {
+		for (Tomate graine : this.getMesTomates()) {
+			if (graine.getDésignation().equals(designation))
+				return graine;
+		}
+		return null;
+	}
+}
